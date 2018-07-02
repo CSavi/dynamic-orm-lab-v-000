8a92c20acc7d9bfd4a3b3edaad1d52019e16a84c
Carrie Savinell <carrielane89@gmail.com>
2018-07-02 14:04:22 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -38,4 +38,10 @@ class InteractiveRecord
   def col_names_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'{send(col_name)}'"
+  end
 end
