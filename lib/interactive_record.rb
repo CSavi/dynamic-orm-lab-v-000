2b6aa3ffda0b0a76d9cf5724bdcd65f8383b20eb
Carrie Savinell <carrielane89@gmail.com>
2018-07-02 13:44:13 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -18,6 +18,11 @@ class InteractiveRecord
     table_info.each do |column|
       column_names << column["name"]
     end
-    column_names.compact   
+    column_names.compact
+  end
+
+  def initialize(options={})
+    options.each do |property, value|
+      self.send()
   end
 end
